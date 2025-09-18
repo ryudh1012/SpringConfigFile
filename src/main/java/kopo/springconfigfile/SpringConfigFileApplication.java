747222d967151a54600d86f81bacf35dde9a7882
ryudh1012 <ryudh1012@gmail.com>
2025-09-18 09:26:53 +0900

    Initial commit

---
+++ src/main/java/kopo/springconfigfile/SpringConfigFileApplication.java
@@ -0,0 +1,13 @@
+package kopo.springconfigfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kopo/springconfigfile/SpringConfigFileApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.springconfigfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
